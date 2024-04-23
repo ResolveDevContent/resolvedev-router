653abb05d4315c792aea95f16b0be8ad113ee8cf
LucasMezza <mezza.lucas12@gmail.com>
2024-04-23 16:29:41 -0300

    file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { lazy, Suspense } from 'react';
-import { Router } from './components/Router';
-import Page404 from './pages/Page404';
-import { Route } from './components/Route';
+import { Router } from './components/Router.jsx';
+import Page404 from './pages/Page404.jsx';
+import { Route } from './components/Route.jsx';
 
 const LazyHomePage = lazy(() => import('./pages/Inicio.jsx'))
 const LazyAboutPage = lazy(() => import('./pages/SobreNosotros.jsx'))
@@ -20,7 +20,6 @@ function App() {
     <main>
       <Suspense fallback={null}>
         <Router routes={appRoutes} defaultComponent={Page404}>
-          <Route path='a don wanna sleep' />
           <Route path='/' component={LazyHomePage} />
           <Route path='/about' component={LazyAboutPage} />
         </Router>      
