77e86b1331af7701a49ef49f4fbe43360acc1ce4
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-25 12:04:56 -0300

    change css

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,7 +4,10 @@ export default function Inicio() {
     return (
         <>
             <div>
-                <h1>Bienvenido a la Home</h1>
+                <h1>HOME</h1>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veritatis facere est natus inventore error. Laborum eos asperiores laboriosam nemo quis deserunt corrupti dolor unde. Laboriosam iusto hic saepe natus?</span>
+                <br />
+                <br />
                 <Link to="/about">Ir a about</Link>
             </div>
         </>
--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -6,7 +6,10 @@ export default function SobreNosotros({ routeParams }) {
     return (
         <>
             <div>
-                <h1>Bienvenido a la About</h1>
+                <h1>ABOUT</h1>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veritatis facere est natus inventore error. Laborum eos asperiores laboriosam nemo quis deserunt corrupti dolor unde. Laboriosam iusto hic saepe natus?</span>
+                <br />
+                <br />
                 <Link to="/">Ir a home</Link>
             </div>
         </>
